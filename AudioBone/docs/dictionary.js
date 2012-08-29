056cdb5a7d09228942c66dc71b1dcc295c307e69
Piotr Czeglik <pczeglik@gmail.com>
2012-08-29 17:27:57 +0200

    Router + nav and some modifications in templates (html, css)

---
+++ AudioBone/docs/dictionary.js
@@ -0,0 +1,19 @@
+/*
+    AUDIOBOOK OBJECT
+    {
+        "Author": "George R.R. Martin",
+        "AverageRating": 9,
+        "BlackBerrySku": "001099Audioteka",
+        "Cycle": "",
+        "IsFreeChapterEnabled": true,
+        "Length": 1873,
+        "ListPrice": 59.9000,
+        "MediumPicture": "http:\/\/audioteka.pl\/images\/products\/george-r-r-martin\/gra-o-tron-srednie.jpg",
+        "ProductId": "gra-o-tron",
+        "Publisher": "ZYSK i S-ka",
+        "Reader": "zespół aktorów",
+        "SampleLink": "http:\/\/audioteka.pl\/content\/samples\/george-r-r-martin\/gra-o-tron.mp3",
+        "SmallPictureLink": "http:\/\/audioteka.pl\/images\/products\/george-r-r-martin\/gra-o-tron-male.jpg",
+        "Title": "Gra o tron"
+    }
+*/
